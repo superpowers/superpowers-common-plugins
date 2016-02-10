10ad314933251c9b8f82e1b89e13343c24fb63e4
Elisée Maurer <elisee@sparklinlabs.com>
2016-02-10 15:48:28 +0100

    textEditorWidget: Clean up and use ES6 in widgetGulpfile.js

--- textEditorWidget/widgetGulpfile.js
+++ textEditorWidget/widgetGulpfile.js
@@ -1,34 +1,30 @@
-var gulp = require("gulp");
-var tasks = [ "stylus", "copy-cm-modes" ];
+const gulp = require("gulp");
+const tasks = [ "stylus", "copy-cm-modes" ];
 
 // Stylus
-var stylus = require("gulp-stylus");
-var concatCss = require("gulp-concat-css");
+const stylus = require("gulp-stylus");
+const concatCss = require("gulp-concat-css");
 
-gulp.task("stylus", function() {
-  return gulp.src("./widget/widget.styl").pipe(stylus({ errors: true, compress: true })).pipe(concatCss("widget.css")).pipe(gulp.dest("./public/"));
-});
+gulp.task("stylus", () => gulp.src("./widget/widget.styl").pipe(stylus({ errors: true, compress: true })).pipe(concatCss("widget.css")).pipe(gulp.dest("./public/")));
 
 // Browserify
-var browserify = require("browserify");
-var vinylSourceStream = require("vinyl-source-stream");
-function makeBrowserify(source, destination, output) {
-  gulp.task(output + "-browserify", function() {
-    var bundler = browserify(source, { standalone: "TextEditorWidget" });
-    bundler.transform("brfs");
-    function bundle() { return bundler.bundle().pipe(vinylSourceStream(output + ".js")).pipe(gulp.dest(destination)); };
-    return bundle();
+const browserify = require("browserify");
+const source = require("vinyl-source-stream");
+
+function makeBrowserify(src, dest, output) {
+  gulp.task(`${output}-browserify`, () => {
+    return browserify(src, { standalone: "TextEditorWidget" })
+      .transform("brfs").bundle()
+      .pipe(source(`${output}.js`))
+      .pipe(gulp.dest(dest));
   });
-  tasks.push(output + "-browserify");
+  tasks.push(`${output}-browserify`);
 }
 
 makeBrowserify("./widget/widget.js", "./public/", "widget");
 
-
 // Copy CodeMirror modes
-gulp.task("copy-cm-modes", function() {
-  return gulp.src([ "node_modules/codemirror/mode/**/*" ]).pipe(gulp.dest("public/codemirror/mode"));
-});
+gulp.task("copy-cm-modes", () => gulp.src([ "node_modules/codemirror/mode/**/*" ]).pipe(gulp.dest("public/codemirror/mode")));
 
 // All
 gulp.task("default", tasks);
